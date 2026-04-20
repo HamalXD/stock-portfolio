9f57c45b2b1339b131521f89fb28a897d08068c3
hamalxd <sammanhamal06@gmail.com>
2026-04-20 22:53:39 +0545

    feat:added routing and stock types

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Dashboard from "./pages/dashboard/Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router";
+
 function App() {
-  return <div className="text-red-900">This App is working</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type TStocks = {
+  id: string;
+  ticker: string;
+  companyName: string;
+  quantity: number;
+  purchasePrice: number;
+  curretnPrice: number;
+  date: string;
+};
